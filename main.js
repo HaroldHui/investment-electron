f00d01324e43171621b6a488aa0215626451b713
Shu Hui <dreamlock0630@gmail.com>
2019-04-07 11:08:38 +1000

    initialize electron app

---
+++ main.js
@@ -0,0 +1,39 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+app.on('ready', ()=> {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  Menu.setApplicationMenu(mainMenu);
+})
+
+
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: 'Add Item'
+      },
+      {
+        label: 'Clear Items'
+      },
+      {
+        label: 'Quit ',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
